d894818901d4a17bf72b92506e78b4985bc1fd8c
Christian Palacios <79600301+chris-palacios@users.noreply.github.com>
2024-01-18 22:39:18 -0800

    Added More Articles to Index
    Added a json file to keep the article data.   Added to the index to populate articles their images and article details

--- routes/index.py
+++ routes/index.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, get_flashed_messages,url_for, Blueprint
+import json
 
 index_blueprint = Blueprint("index_blueprint", __name__)
 
 @index_blueprint.route("/")
 @index_blueprint.route("/index")
 def index():
-    return render_template("index.html")
+    json_file= "C:\\Users\\links\\Documents\\GitHub\\SharksWebsite\\data\\articles.json"
+    with open(json_file) as json_data:
+        article = json.load(json_data)
+        
+    return render_template("index.html", article=article)
